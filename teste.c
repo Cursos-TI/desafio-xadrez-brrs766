1e08158facbcbb05c6ddb41d7d421573d8171d4d
brrs766 <br1597431@gmail.com>
2025-04-29 17:35:47 +0000

    commit 3

--- teste.c
+++ teste.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
  
 int main() {
-    int num;
+    int option;
    
     do {
-        printf("Digite um número (negativo para sair): ");
-        scanf("%d", &num);
+        printf("Menu:\n");
+        printf("1. Opção 1\n");
+        printf("2. Opção 2\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opção: \n");
+        scanf("%d", &option);
        
-        if (num >= 0) {
-            printf("Você digitou: %d\n", num);
+        switch(option) {
+            case 1:
+                printf("Você escolheu a opção 1\n");
+                break;
+            case 2:
+                printf("Você escolheu a opção 2\n");
+                break;
+            case 3:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
         }
-    } while (num >= 0);
-   
-    printf("Número negativo detectado. Saindo...\n");
+    } while (option != 3);
    
     return 0;
 }
